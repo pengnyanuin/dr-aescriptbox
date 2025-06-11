37589d56727f600f85f97c014d0023c74c2dfcfd
Pengnyanuin <mats.penguin@gmail.com>
2025-06-12 01:39:09 +0200

    mask color script

--- client/index.js
+++ client/index.js
@@ -8,6 +8,9 @@ openButton.addEventListener('click', openDoc);
 const testButton = document.querySelector("#test-button");
 testButton.addEventListener('click', testDoc);
 
+const maskColorButton = document.querySelector("#script-mask-color");
+maskColorButton.addEventListener('click', maskColor);
+
 /* 3) Write a helper function to pass instructions to the ExtendScript side. */
 function openDoc() {
     csInterface.evalScript('listScripts()', (result) => {
@@ -19,5 +22,12 @@ function openDoc() {
 function testDoc() {
     csInterface.evalScript('runTest()', (result) => {
         document.getElementById('debug').innerText = 'DONE';
+    });
+}
+
+function maskColor() {
+    csInterface.evalScript('maskColorScript()', (result) => {
+        const jsonResult = JSON.parse(result);
+        document.getElementById('debug').innerText = jsonResult;
     });
 }
