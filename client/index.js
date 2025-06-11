310bd242c01c4ed2909ddf5fbd0dbca72fe5ffbf
Pengnyanuin <mats.penguin@gmail.com>
2025-06-11 03:36:35 +0200

    testing opening scripts

--- client/index.js
+++ client/index.js
@@ -5,9 +5,19 @@ const csInterface = new CSInterface();
 const openButton = document.querySelector("#open-button");
 openButton.addEventListener('click', openDoc);
 
+const testButton = document.querySelector("#test-button");
+testButton.addEventListener('click', testDoc);
+
 /* 3) Write a helper function to pass instructions to the ExtendScript side. */
 function openDoc() {
-    csInterface.evalScript('openDocument()', (result) => {
+    csInterface.evalScript('listScripts()', (result) => {
+        const jsonResult = JSON.parse(result);
         document.getElementById('debug').innerText = result;
+    });
+}
+
+function testDoc() {
+    csInterface.evalScript('runTest()', (result) => {
+        document.getElementById('debug').innerText = 'DONE';
     });
 }
