b1dc7e136e57fba73a7bf4eb394e4f5da4bce80b
Pengnyanuin <mats.penguin@gmail.com>
2025-06-21 06:53:13 +0200

    open most recent project description and options

--- scripts/openMostRecentProject.jsx
+++ scripts/openMostRecentProject.jsx
@@ -1,11 +1,18 @@
 (function () {
-    // This works for Adobe AE 25.0.0
-    // If it doesn't work you can figure out your versions
-    // * by navigating to %appdata%\Adobe\After Effects\[YOUR_AE_VERSION]\
-    // * Open "Adobe After Effects 25.0 Prefs.txt" and find reference to what your most recent project is
-    // * First value in next line is title of the category where this is located, likely will include MRU somewhere
-    // * Second value is directly the value where the path to your most recent project file is assigned to
-    var recentFile = app.preferences.getPrefAsString("Most Recently Used (MRU) Search v2", "MRU Project Path ID # 0, File Path");
+    /**
+     * Open last opened project file
+     *
+     * These values are tested and work for Adobe AE 25.x
+     * If it doesn't work for you, you can figure out your versions
+     * * by navigating to %appdata%\Adobe\After Effects\[YOUR_AE_VERSION]\
+     * * Open "Adobe After Effects 25.0 Prefs.txt" and find reference to what your most recent project is
+     * * sectionName is title of the section where this is located, likely will include MRU somewhere in the string
+     * * keyName is directly the value where the path to your most recent project file is assigned to
+     */
+    var sectionName = 'Most Recently Used (MRU) Search v2';
+    var keyName = 'MRU Project Path ID # 0, File Path';
+
+    var recentFile = app.preferences.getPrefAsString(sectionName, keyName);
 
     if (recentFile) {
         var recentProject = new File(recentFile);
